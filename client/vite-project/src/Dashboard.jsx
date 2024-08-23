9dec4373af3de3429c5aba2f9fab1acd883be9b1
Pósán Róbert <posanrobi@gmail.com>
2024-08-24 00:44:53 +0200

    Fade MUI

--- client/vite-project/src/Dashboard.jsx
+++ client/vite-project/src/Dashboard.jsx
@@ -7,6 +7,7 @@ import {
   Typography,
   Container,
   Box,
+  Fade,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { deleteUser, fetchUsers } from "./api";
@@ -71,159 +72,177 @@ const Dashboard = () => {
         alignItems: "center",
       }}
     >
-      {isEditingUser ? (
-        <EditUser
-          setIsEditingUser={setIsEditingUser}
-          userId={editingUserId}
-          onUserUpdated={handleUserUpdated}
-        />
-      ) : isCreatingUser ? (
-        <CreateUser
-          setIsCreatingUser={setIsCreatingUser}
-          onUserCreated={handleUserCreated}
-        />
-      ) : (
-        <Box
-          sx={{
-            background: "#fff",
-            padding: "1rem",
-            borderRadius: "0.5rem",
-            minHeight: "30rem",
-            width: "38rem",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <List
-            sx={{
-              maxHeight: "26rem",
-              overflow: "auto",
-              overflowX: "none",
-              padding: "0 1rem 0 1rem",
-              display: "grid",
-              gridTemplateColumns:
-                users.length === 0 ? "unset" : "repeat(2, 1fr)",
-              gap: "1rem",
-              alignItems: "start",
-            }}
-          >
-            {users.length === 0 ? (
-              <Typography
-                color={"black"}
-                sx={{
-                  fontSize: "1.2rem",
-                  color: "grey",
-                  marginTop: "1rem",
-                }}
-              >
-                No user available
-              </Typography>
-            ) : (
-              users.map((user) => (
-                <ListItem
-                  key={user._id}
+      <Box
+        sx={{
+          background: "#fff",
+          padding: "1rem",
+          borderRadius: "0.5rem",
+          minHeight: "30rem",
+          width: "38rem",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Fade in={isEditingUser || isCreatingUser} unmountOnExit timeout={500}>
+          <Box sx={{ position: "absolute" }}>
+            {isEditingUser && (
+              <EditUser
+                setIsEditingUser={setIsEditingUser}
+                userId={editingUserId}
+                onUserUpdated={handleUserUpdated}
+              />
+            )}
+            {isCreatingUser && (
+              <CreateUser
+                setIsCreatingUser={setIsCreatingUser}
+                onUserCreated={handleUserCreated}
+              />
+            )}
+          </Box>
+        </Fade>
+
+        {!isEditingUser && !isCreatingUser && (
+          <>
+            <List
+              sx={{
+                maxHeight: "26rem",
+                overflow: "auto",
+                overflowX: "none",
+                padding: "0 1rem 0 1rem",
+                display: "grid",
+                gridTemplateColumns:
+                  users.length === 0 ? "unset" : "repeat(2, 1fr)",
+                gap: "1rem",
+                alignItems: "start",
+              }}
+            >
+              {users.length === 0 ? (
+                <Typography
+                  color={"black"}
                   sx={{
-                    backgroundColor: "#f1f1f1",
-                    padding: "1rem",
-                    borderRadius: 3,
-                    width: "18rem",
-                    height: "8rem",
-                    display: "flex",
-                    flexDirection: "column",
+                    fontSize: "1.2rem",
+                    color: "grey",
+                    marginTop: "1rem",
                   }}
                 >
-                  <ListItemText
-                    primary={`${user.name} (${user.age})`}
-                    primaryTypographyProps={{
-                      sx: { color: "#000", width: "100%", textAlign: "center" },
-                    }}
-                  />
-                  <Box
+                  No user available
+                </Typography>
+              ) : (
+                users.map((user) => (
+                  <ListItem
+                    key={user._id}
                     sx={{
+                      backgroundColor: "#f1f1f1",
+                      padding: "0.5rem",
+                      borderRadius: 3,
+                      width: "18rem",
+                      height: "8rem",
                       display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      width: "100%",
-                      maxWidth: "15rem",
+                      flexDirection: "column",
                     }}
                   >
-                    <Typography variant="body2" color="textSecondary">
-                      {user.email}
-                    </Typography>
-                    <Box>
+                    <ListItemText
+                      primary={`${user.name} (${user.age})`}
+                      primaryTypographyProps={{
+                        sx: {
+                          color: "#000",
+                          width: "100%",
+                          textAlign: "center",
+                        },
+                      }}
+                    />
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        width: "100%",
+                        maxWidth: "15rem",
+                      }}
+                    >
                       <Typography variant="body2" color="textSecondary">
-                        Gender:{" "}
-                        {user.gender === "male"
-                          ? "Male"
-                          : user.gender === "female"
-                          ? "Female"
-                          : "Other"}
+                        {user.email}
                       </Typography>
+
+                      <Box>
+                        <Typography variant="body2" color="textSecondary">
+                          Gender:{" "}
+                          {user.gender === "male"
+                            ? "Male"
+                            : user.gender === "female"
+                            ? "Female"
+                            : "Other"}
+                        </Typography>
+                      </Box>
                     </Box>
-                  </Box>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      width: "100%",
-                    }}
-                  >
-                    <IconButton
-                      onClick={() => handleEditUser(user._id)}
+
+                    <Box
                       sx={{
-                        "&:hover": {
-                          color: "darkgrey",
-                          backgroundColor: "transparent",
-                        },
-                        "&:focus": {
-                          outline: "none",
-                        },
-                        "&:active": {
-                          boxShadow: "none",
-                        },
+                        display: "flex",
+                        justifyContent: "space-between",
+                        width: "100%",
                       }}
                     >
-                      <RiEdit2Fill />
-                    </IconButton>
-                    <IconButton
-                      onClick={() => handleDeleteUser(user._id)}
-                      sx={{
-                        color: "red",
-                        "&:hover": {
-                          color: "darkred",
-                          backgroundColor: "transparent",
-                        },
-                        "&:focus": {
-                          outline: "none",
-                        },
-                        "&:active": {
-                          boxShadow: "none",
-                        },
-                      }}
-                    >
-                      <TbTrashXFilled />
-                    </IconButton>
-                  </Box>
-                </ListItem>
-              ))
-            )}
-          </List>
-          <Button
-            onClick={() => setIsCreatingUser(true)}
-            variant="contained"
-            sx={{
-              background: "#585151",
-              marginTop: "1rem",
-              width: "12rem",
-              "&:hover": { background: "#484141" },
-            }}
-          >
-            Create a new user
-          </Button>
-        </Box>
-      )}
+                      <IconButton
+                        onClick={() => handleEditUser(user._id)}
+                        sx={{
+                          "&:hover": {
+                            color: "darkgrey",
+                            backgroundColor: "transparent",
+                          },
+                          "&:focus": {
+                            outline: "none",
+                          },
+                          "&:active": {
+                            boxShadow: "none",
+                          },
+                        }}
+                      >
+                        <RiEdit2Fill />
+                      </IconButton>
+
+                      <IconButton
+                        onClick={() => handleDeleteUser(user._id)}
+                        sx={{
+                          color: "red",
+                          "&:hover": {
+                            color: "darkred",
+                            backgroundColor: "transparent",
+                          },
+                          "&:focus": {
+                            outline: "none",
+                          },
+                          "&:active": {
+                            boxShadow: "none",
+                          },
+                        }}
+                      >
+                        <TbTrashXFilled />
+                      </IconButton>
+                    </Box>
+                  </ListItem>
+                ))
+              )}
+            </List>
+
+            <Button
+              onClick={() => setIsCreatingUser(true)}
+              variant="contained"
+              sx={{
+                background: "#585151",
+                marginTop: "1rem",
+                width: "12rem",
+                "&:hover": { background: "#484141" },
+              }}
+            >
+              Create a new user
+            </Button>
+          </>
+        )}
+      </Box>
+
       <Statistics />
     </Container>
   );
